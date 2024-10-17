c3fd7f3657d5fe7162db25d98ed4300238157340
YasarDogann <yasardgn99@gmail.com>
2024-10-17 14:07:28 +0300

    library

---
+++ patika_w4_patikaLibraryApp/Kitaplar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_patikaLibraryApp
+{
+    public  class Kitaplar
+    {
+        //Toplam oluşturulan kitap nesnesini tutan statik alan
+        private static int bookCount = 0; 
+
+        // Fields
+        private string bookName;
+        private string authorName;
+        private string authorSurname;
+        private int pageCount;
+        private string publishingHouse;
+        private DateTime publishingDate;
+
+
+        //Kitap bilgisine erişim 
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname {  get; set; }
+        public int PageCount { get; set; }
+        public string PublishingHouse { get; set; }
+        public DateTime PublishingDate { get { return publishingDate; } private set { publishingDate = value; } }
+
+        // Parametresiz Varsasıyaln Yapıcı Method
+        public Kitaplar()
+        {
+            publishingDate = DateTime.Now; //kayıt tarihi otomatik olarak nesne oluşturulunca atanıyor o zamanki zaman'a göre 
+            bookCount++;
+        }
+        
+        // Parametreli Yapıcı Method ve this() varsayılan yapıcı methodu çağırıyor
+        public Kitaplar (string _bookName, string _authorName, string _authorSurname, int _pageCount, string _publishingHouse): this()//sınıfın başka constructur'ı çağırıldı
+        {
+            BookName = _bookName;   //kitap adı atanır
+            AuthorName = _authorName; //yazar adı atanır
+            AuthorSurname = _authorSurname; //yazar soyadı atanır
+            PageCount = _pageCount; // sayfa sayısı atanır
+            PublishingHouse = _publishingHouse; // yayınevi atanır
+            //PublishingDate = _publishingDate;     datetime koymadım çünkü default yapıcı methodu çağırdım
+        }
+
+        //kitap detaylarını ekrana yazdıran methd
+        public void Display()
+        {
+            Console.WriteLine($"Kitap Adı: {BookName}");
+            Console.WriteLine($"Yazar Adı: {AuthorName}");
+            Console.WriteLine($"Yazar Soyadı: {AuthorSurname}");
+            Console.WriteLine($"Sayfa Sayısı: {PageCount}");
+            Console.WriteLine($"Yayınevi: {PublishingHouse}");
+            Console.WriteLine($"Yayın Tarihi: {PublishingDate}");
+        }
+        
+        //oluşturulan kitap sayısını döndüren static method
+        public static int GetBookCount()
+        {
+            return bookCount;
+        }
+
+
+    }
+}
---
+++ patika_w4_patikaLibraryApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+
+namespace patika_w4_patikaLibraryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kitap sınıfından book1 adında nesne oluşturuldu ve varsayılan Parametresiz Yapıcı method kullanıldı
+            Kitaplar book1 = new Kitaplar();
+            book1.BookName = "Olasılıksız";
+            book1.AuthorName = "Adam";
+            book1.AuthorSurname = "Fawer";
+            book1.PageCount = 500;
+            book1.PublishingHouse = "Falan Filan Kitabevi";
+            book1.Display(); //book1'in bilgilerini ekrana gösterdik
+
+            Console.WriteLine("\r\n---------------------------------------------------------------\r\n");
+
+            // Book2 adında bir nesne daha oluşturuyoruz ve 2. Yapıcı method Parametreli olan kullanıldı 
+            Kitaplar book2 = new Kitaplar("1984", "George","Orwell", 250, "Dümenden Kitabevi");
+            book2.Display();
+
+            Console.WriteLine("\r\n---------------------------------------------------------------\r\n");
+
+            // book3 adında bir nesne daa oluşturduk parametreli yapıcı method ile
+            Kitaplar book3 = new Kitaplar("Adı Aylin", "Ayşe", "Kulin", 389, "Remzi Kitabevi");
+            book3.Display();
+
+            Console.WriteLine("\r\n*************************************************************\r\n");
+
+            //oluşturulan kitap nesnesinin sayısını görebilmek için statik method çağırıldı
+            Console.WriteLine("Oluşturulan Kitap Sayısı: {0}",Kitaplar.GetBookCount());
+
+
+
+
+            #region Class, Property, New, Constructor Açıklama
+            /**
+             * Class: OOP'de benzer özelliklere ve işlevselliklere sahip nesneleri tanımlamak için kullanılır ÖRnek: Kitaplar
+             * Property: Nesne'nin özelliklerine erişim sağlayan yoldur Örnek Kitabın adı veya sayfa sayısı gibi bütün kitaplarda ortaktır
+             * New : Yeni bir nesne oluşturmak için kullanılan anahtar kelimedir.
+             * Constuructor: sınıfın bir nesnesi oluşturulunca otomatik olarak çağırılan bir methoddur. Başlangıç değeri oluşturmada kullanılır             
+             */
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
+ 
